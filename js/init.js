6e42c3205f1c06d2f67740097a7a743b7af33af7
Miguel Rodrigues <14356@alunos.ipbeja.pt>
2016-11-13 00:04:24 +0000

    Toast

--- js/init.js
+++ js/init.js
@@ -5,8 +5,15 @@
     $('.scrollspy').scrollSpy();
     $('.collapsible').collapsible({
       accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
-      Materialize.toast('I am a toast!', 4000)
     });
+
+    $("#toast").click(function () {
+    	var $toastContent = 'A sua mensagem foi enviada!';
+    	  Materialize.toast($toastContent, 4000) // 4000 is the duration of the toast
+    	/* body... */
+    });
+
+
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
