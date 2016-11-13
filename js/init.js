725f7b9b65d0c81a886322c107d52a9d0f80b4f9
Miguel Rodrigues <14356@alunos.ipbeja.pt>
2016-11-13 17:38:44 +0000

    Condições jQuery para envio de mensagem

--- js/init.js
+++ js/init.js
@@ -1,19 +1,23 @@
 (function($){
   $(function(){
-
     $('.button-collapse').sideNav();
     $('.scrollspy').scrollSpy();
     $('.collapsible').collapsible({
       accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
     });
 
+    
     $("#toast").click(function () {
-    	var $toastContent = 'A sua mensagem foi enviada!';
-    	  Materialize.toast($toastContent, 4000) // 4000 is the duration of the toast
-    	/* body... */
+    	var nameValue = $("#last_name").val();
+		var textValue = $("#textarea1").val();
+		var subjectValue = $("#subjectText").val();
+    	if((textValue.length > 0) && (subjectValue.length > 0) && (nameValue.length > 0))
+    	{
+    		var $toastContent = 'A sua mensagem foi enviada!';
+    	  	Materialize.toast($toastContent, 4000)
+    	}
+
     });
-
-
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
